23099027c0a843c200501ed7c3b7bc95b1eee081
Chee How Yap <yho@sitecore.net>
2018-04-13 15:44:19 +0800

    Commit Patch

---
+++ src/Sitecore.Support.95337/Mvc/Pipelines/HttpRequest/TransferMvcLayout.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.Mvc.Pipelines.HttpRequest
+{
+  using Sitecore.Diagnostics;
+  using Sitecore.Mvc.Configuration;
+  using Sitecore.Pipelines.HttpRequest;
+  using Sitecore.Security.Authentication;
+  using System;
+  using System.IO;
+  using System.Web;
+
+  [UsedImplicitly]
+	public class TransferMvcLayout : HttpRequestProcessor
+	{
+		public override void Process(HttpRequestArgs args)
+		{
+			Assert.ArgumentNotNull(args, "args");
+			string filePath = Context.Page.FilePath;
+			if (!string.IsNullOrWhiteSpace(filePath))
+			{
+				string extension;
+				try
+				{
+					extension = Path.GetExtension(filePath);
+				}
+				catch (ArgumentException)
+				{
+					extension = string.Empty;
+				}
+				if (MvcSettings.IsViewExtension(extension))
+				{
+					HttpContext current = HttpContext.Current;
+					if (!Context.PageMode.IsNormal && AuthenticationHelper.IsAuthenticationTicketExpired())
+					{
+						current.Response.Redirect("/sitecore/login");
+					}
+					Tracer.Info("MVC Layout detected - transfering to ASP.NET MVC");
+					args.Context.Items["sc::IsContentUrl"] = "true";
+					args.AbortPipeline();
+				}
+			}
+		}
+	}
+}
